cbccee853a5059356c30095581149a91efc58d4d
shiv-promantia <shiv.bahadur@promantia.com>
2026-09-24 16:52:30 +0530

    Updated the financial location value

--- src/main/java/com/promanatia/CamelDemo/transformation/openbravo/payment/impl/ItemFinancialLocationRule.java
+++ src/main/java/com/promanatia/CamelDemo/transformation/openbravo/payment/impl/ItemFinancialLocationRule.java
@@ -1,4 +1,4 @@
-package com.promanatia.CamelDemo.transformation.openbravo.salesinvoice.impl;
+package com.promanatia.CamelDemo.transformation.openbravo.payment.impl;
 
 import org.springframework.stereotype.Component;
 
@@ -9,17 +9,17 @@ import com.promanatia.CamelDemo.transformation.TransformationRule;
 import com.promanatia.CamelDemo.utility.RowContext;
 
 @Component
-public class ItemLineLocationRule implements TransformationRule {
+public class ItemFinancialLocationRule implements TransformationRule {
 
 	private final LookupService lookupService;
 
-	public ItemLineLocationRule(LookupService lookupService) {
+	public ItemFinancialLocationRule(LookupService lookupService) {
 		this.lookupService = lookupService;
 	}
 
 	@Override
 	public String getRuleCode() {
-		return "ITEM_LINE_LOCATION";
+		return "ITEM_FINANCIAL_LOCATION";
 	}
 
 	@Override
@@ -30,7 +30,7 @@ public class ItemLineLocationRule implements TransformationRule {
 			dto = lookupService.getOrganization(row.get("organization"));
 			row.setOrganization(dto);
 		}
-		return dto == null ? "" : dto.getItemLineLocation();
+		return dto == null ? "" : dto.getFinancialLocation();
 	}
 
 }
